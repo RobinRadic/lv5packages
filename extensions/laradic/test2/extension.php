644fd9820b028b807b44d81f22d007826397f53c
robinradic <robin@radic.nl>
2015-03-24 06:06:14 +0100

    before remove ignore files

---
+++ extensions/laradic/test2/extension.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Contracts\Foundation\Application;
+use Laradic\Extensions\Extension;
+use Laradic\Extensions\ExtensionCollection;
+
+return array(
+    'name' => 'Test',
+    'slug' => 'laradic/test',
+    'dependencies' => [],
+    'register' => function(Application $app, Extension $extension, ExtensionCollection $extensions)
+    {
+
+    },
+    'boot' => function(Application $app, Extension $extension, ExtensionCollection $extensions)
+    {
+
+    }
+);
